c7017be9a876e17424cfe447005f6ccf8b43f011
Luke Taylor <luke@deentaylor.com>
2018-04-08 23:24:47 -0400

    Configure webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,53 @@ module.exports = {
     filename: 'index.js',
   },
 
+  module: {
+    rules: [
+      // SASS files
+      {
+        test: /\.sass$/,
+        loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      // CSS files
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      // Vue template files
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
+        },
+      },
+      // JavaScript files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      // HTML files
+      {
+        test: /\.html$/,
+        loaders: [
+          'file-loader?name=[path][name].html&context=src',
+          'extract-loader',
+          'html-loader',
+        ],
+      },
+      // Files that require no compilation or processing
+      {
+        test: /\.(ttf|woff|woff2|eot|png|svg)/,
+        loader: 'url-loader',
+        query: { limit: 10000, name: '[path][name].[ext]', context: 'src' },
+      },
+    ],
+  },
+
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     noInfo: true,
