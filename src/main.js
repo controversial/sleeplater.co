2ba738c126490fce687297f497c23815854c0e71
Luke Taylor <luke@deentaylor.com>
2018-04-15 10:58:30 -0400

    setup on DOMContentLoaded to avoid visible adjustment as much as possible

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import textSplit from './textSplit';
 
 window.textSplit = textSplit;
-window.addEventListener('load', () => {
-  textSplit.setup();
-});
+// Set up as soon as possible
+document.addEventListener('DOMContentLoaded', () => textSplit.setup());
+// Adjust widths again when fonts are loaded just in case
+window.addEventListener('load', () => textSplit.setup());
