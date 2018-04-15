8ae3cce55233a0aa20f76af940deaf71f4a1cbe9
Luke Taylor <luke@deentaylor.com>
2018-04-15 11:37:50 -0400

    Fill progress bar on page load

--- src/main.js
+++ src/main.js
@@ -5,3 +5,5 @@ window.textSplit = textSplit;
 document.addEventListener('DOMContentLoaded', () => textSplit.setup());
 // Adjust widths again when fonts are loaded just in case
 window.addEventListener('load', () => textSplit.setup());
+// Complete progress bar when page loads
+window.addEventListener('load', () => setTimeout(() => { textSplit.progress = 1; }, 500));
