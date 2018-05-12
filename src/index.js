dbe3d3f37782462b9029739ad7ac484b903e67b1
Luke Taylor <luke@deentaylor.com>
2018-05-12 16:49:51 -0400

    Add automatic slide down/up transitions for pages in main scroll

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import router from './router';
 
 import Vue from 'vue';
 
+import routes from './pages';
+const primaryRoutes = routes.filter(route => route.meta.primary);
+const primaryRouteNames = primaryRoutes.map(route => route.name);
+
 window.app = new Vue({
   el: '#app',
   router,
@@ -26,7 +30,16 @@ window.app = new Vue({
     // Vue route changed
     $route(to, from) {
       if (from.name === 'home' && to.name === 'shop') [this.transitionName, this.transitionMode] = ['period-scale', 'out-in'];
-      else [this.transitionName, this.transitionMode] = ['', ''];
+      else if (primaryRouteNames.includes(from.name) && primaryRouteNames.includes(to.name)) {
+        if (primaryRouteNames.indexOf(from.name) > primaryRouteNames.indexOf(to.name)) { // Going up
+          [this.transitionName, this.transitionMode] = ['slide-downwards', ''];
+        } else { // Going down
+          [this.transitionName, this.transitionMode] = ['slide-upwards', ''];
+        }
+      } else {
+        [this.transitionName, this.transitionMode] = ['', ''];
+      }
+      console.log(this.transitionName);
     },
   },
 });
