c3ff94db1f882df61057e1e30760bf7398249181
Luke Taylor <luke@deentaylor.com>
2018-05-12 13:14:06 -0400

    Register components for all the feather icons I'm planning to use

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,17 @@
+/* eslint-disable import/first */
 import Vue from 'vue';
+
+// Feather icons
+import { ChevronUpIcon, ChevronDownIcon, BarChartIcon, GridIcon, ShoppingCartIcon, XIcon, InstagramIcon, TwitterIcon, MailIcon } from 'vue-feather-icons';
+Vue.component('chevron-up-icon', ChevronUpIcon);
+Vue.component('chevron-down-icon', ChevronDownIcon);
+Vue.component('menu-icon', BarChartIcon); // Goes sideways
+Vue.component('grid-icon', GridIcon);
+Vue.component('shopping-cart-icon', ShoppingCartIcon);
+Vue.component('close-icon', XIcon);
+Vue.component('instagram-icon', InstagramIcon);
+Vue.component('twitter-icon', TwitterIcon);
+Vue.component('mail-icon', MailIcon);
 
 import Loader from './loader/loader.vue';
 Vue.component('loader', Loader);
