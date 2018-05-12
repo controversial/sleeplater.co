b6fdc9b6d406862bd6b9d30a869de38cee5d0fb7
Luke Taylor <luke@deentaylor.com>
2018-05-12 07:50:51 -0400

    Fade in shop contents after transition

--- src/pages/shop/shop.vue
+++ src/pages/shop/shop.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Hello</div>
+  <div class="shop">
+    <div class="contents">
+      Hello
+    </div>
+  </div>
 </template>
+
+<style scoped lang="sass">
+@import ../../colors
+
+.shop
+  background: $actually-white
+  color: $dark-0
+</style>
