b73cb486b018304fd68f0b207d9f294b378db773
Luke Taylor <luke@deentaylor.com>
2018-05-12 07:29:57 -0400

    add shop page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 export default [
   {
+    name: 'home',
     path: '/',
     component: require('./home/home.vue').default,
+  },
+  {
+    name: 'shop',
+    path: '/shop',
+    component: require('./shop/shop.vue').default,
   },
 ];
