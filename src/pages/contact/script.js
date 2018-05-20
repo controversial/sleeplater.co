a6ceae3e95e5e92b724220e58a6a4b8cfd808fa5
Luke Taylor <luke@deentaylor.com>
2018-05-20 14:08:51 -0400

    Persist contact form state in vuex

--- src/pages/contact/script.js
+++ src/pages/contact/script.js
@@ -1,8 +1,21 @@
 export default {
-  data: () => ({
-    name: '',
-    email: '',
-    phone: '',
-    message: '',
-  }),
+  // getters/setters so that each item can be reflected persistently in vuex
+  computed: {
+    name: {
+      get() { return this.$store.state.contactForm.name; },
+      set(value) { this.$store.commit({ type: 'updateContactForm', item: 'name', value }); },
+    },
+    email: {
+      get() { return this.$store.state.contactForm.email; },
+      set(value) { this.$store.commit({ type: 'updateContactForm', item: 'email', value }); },
+    },
+    phone: {
+      get() { return this.$store.state.contactForm.phone; },
+      set(value) { this.$store.commit({ type: 'updateContactForm', item: 'phone', value }); },
+    },
+    message: {
+      get() { return this.$store.state.contactForm.message; },
+      set(value) { this.$store.commit({ type: 'updateContactForm', item: 'message', value }); },
+    },
+  },
 };
--- src/store.js
+++ src/store.js
@@ -6,8 +6,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     loaded: false,
+    contactForm: {
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    },
   },
   mutations: {
     loaded(state) { state.loaded = true; },
+    updateContactForm(state, payload) { state.contactForm[payload.item] = payload.value; },
   },
 });
