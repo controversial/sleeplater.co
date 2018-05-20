006ce4890859bc1b49678e4072a1458c75539727
Luke Taylor <luke@deentaylor.com>
2018-05-20 17:37:45 -0400

    Add slack bot function for constructing formatted messages

--- slackbot/index.js
+++ slackbot/index.js
@@ -11,3 +11,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.set('json spaces', 2);
+
+function constructSlackMessage(name, email, phone, message) {
+  return {
+    attachments: [{
+      pretext: 'New message from contact form:',
+      author_name: name,
+      author_link: `mailto:${email}`,
+      text: message,
+      fields: [
+        { title: 'Email', value: email, short: true },
+        phone ? { title: 'Phone number', value: phone, short: true } : undefined,
+      ],
+      fallback: `Message from ${name} (${email}, ${phone}): ${message}.`,
+    }],
+  };
+}
