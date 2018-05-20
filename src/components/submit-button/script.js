4c79d952fbb7b6bff7825d5be771efa79843ceba
Luke Taylor <luke@deentaylor.com>
2018-05-20 19:09:42 -0400

    Scaffold methods for new states of submit-button

---
+++ src/components/submit-button/script.js
@@ -0,0 +1,7 @@
+export default {
+  methods: {
+    load() {},
+    complete() {},
+    fail() {},
+  },
+};
--- src/components/submit-button/submit-button.vue
+++ src/components/submit-button/submit-button.vue
@@ -4,4 +4,5 @@
   </button>
 </template>
 
+<script src="./script.js"></script>
 <style scoped lang="sass" src="./style.sass"></style>
