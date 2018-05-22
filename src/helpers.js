0cd79389863153f5ccbb30089c26b68bb7f560e9
Luke Taylor <luke@deentaylor.com>
2018-05-21 20:28:14 -0400

    Add "pick" function to helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,11 @@
+// Return a promise that resolves after a given time (useful for await)
 export function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Return a copy of an object that contains only some specified keys from the first object
+export function pick(object, keys) {
+  const out = {};
+  keys.forEach((k) => { out[k] = object[k]; });
+  return out;
 }
