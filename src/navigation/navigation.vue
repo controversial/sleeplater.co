7b989ded623308caeb494d16eed29e9bf1ab45bd
Luke Taylor <luke@deentaylor.com>
2018-05-22 21:29:07 -0400

    Shrink when menu button pressed

--- src/navigation/navigation.vue
+++ src/navigation/navigation.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="navigation">
     <!-- Nav controls -->
-    <menu-button v-bind:color="navIconColor" v-bind:transition-delay="navTransitionDelay"></menu-button>
+    <menu-button
+      v-on:click="shrunken = !shrunken"
+      v-bind:color="navIconColor" v-bind:transition-delay="navTransitionDelay"
+    ></menu-button>
     <nav-buttons ref="nav"
       v-bind:color="navIconColor" v-bind:transition-delay="navTransitionDelay"
       v-bind:canGoUp="canGoUp" v-bind:canGoDown="canGoDown"
