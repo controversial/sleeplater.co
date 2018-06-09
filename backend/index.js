c10fa1ec21e7d292151ffdea0c7d7a80ddbd9d9c
Luke Taylor <luke@deentaylor.com>
2018-06-09 16:11:33 -0400

    Create products endpoint in backend

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,27 @@ app.get('/', (req, res) => {
   res.send('ok');
 });
 
+app.get('/products', async (req, res) => {
+  const r = await makeReq(`/accounts/${accountId}/products`);
+  function findOption(optionId) {
+    // Build array of "options" objects that were attached to the response
+    return r.included
+      .filter(e => e.type === 'product_options')
+      .map(o => ({ id: o.id, name: o.attributes.name, price: o.attributes.price }))
+      .find(o => o.id === optionId);
+  }
+  const products = r.data.map(p => ({
+    id: p.id,
+    name: p.attributes.name,
+    status: p.attributes.status,
+    description: p.attributes.description,
+    prince: p.attributes.default_price,
+    image: p.attributes.primary_image_url,
+    options: p.relationships.options.data.map(option => findOption(option.id)),
+  }));
+  res.send(products);
+});
+
 
 // Run server
 const args = process.argv.slice(2);
