ff474feefda457d0ab1c2949653da57676588201
Luke Taylor <luke@deentaylor.com>
2018-06-10 13:52:45 -0400

    Include basic info in shop-item

--- src/components/shop-item/script.js
+++ src/components/shop-item/script.js
@@ -1,2 +1,10 @@
 export default {
+  props: ['id', 'name', 'status', 'description', 'price', 'image', 'options'],
+  computed: {
+    formattedPrice() {
+      const num = parseFloat(this.price);
+      if (Math.floor(num) === num) return `$${num}`;
+      return `$${num.toFixed(2)}`;
+    },
+  },
 };
--- src/components/shop-item/shop-item.vue
+++ src/components/shop-item/shop-item.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="shop-item">
+    <img v-bind:src="image" alt="Product preview">
+    <div class="info">
+      <div class="title">{{name}}</div>
+      <div class="price">{{formattedPrice}}</div>
+    </div>
+  </div>
 </template>
 
 <script src="./script.js"></script>
