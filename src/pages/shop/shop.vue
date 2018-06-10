0c8ca3a5c05d9c5f12275d1c1c891014a07cb959
Luke Taylor <luke@deentaylor.com>
2018-06-10 11:30:35 -0400

    replace .contents container with .fade-in class for period scale animation

--- src/pages/shop/shop.vue
+++ src/pages/shop/shop.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="page">
-    <div class="contents">
-      <h1 class="bg-title">
-        sleep later
-      </h1>
-    </div>
+    <h1 class="bg-title fade-in">sleep later</h1>
   </div>
 </template>
 
