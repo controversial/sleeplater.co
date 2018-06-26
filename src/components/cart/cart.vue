34b5f6ed6559eab6a3d8d4b37597fc86cd6704a7
Luke Deen Taylor <luke@deentaylor.com>
2018-06-25 22:16:12 -0400

    add summary of cart contents to cart component

--- src/components/cart/cart.vue
+++ src/components/cart/cart.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="cart" v-bind:class="{ hidden }">
     <div class="contents">
+
       <close-icon v-on:click="$emit('close')"></close-icon>
+
       <h1>Cart</h1>
+
+      <div class="summary">
+        <span>{{itemsCount}} item{{itemsCount === 1 ? '' : 's'}}</span>
+        <div class="separator"></div>
+        <span>${{ subtotal }}</span>
+      </div>
+
     </div>
   </div>
 </template>
