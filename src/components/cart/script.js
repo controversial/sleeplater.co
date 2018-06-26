76af394951b62ad5cd7d77b9002c5b7b2ef3d389
Luke Deen Taylor <luke@deentaylor.com>
2018-06-25 22:04:16 -0400

    add "subtotal" computed property to cart

--- src/components/cart/script.js
+++ src/components/cart/script.js
@@ -1,3 +1,17 @@
 export default {
   props: ['hidden'],
+
+  computed: {
+    subtotal() {
+      return this.$store.state.cart
+        // Calculate price for each cart item
+        .map(({ id, option, quantity }) => {
+          const product = this.$store.state.products.find(p => p.id === id);
+          const optionPrice = product.options[option];
+          return optionPrice * quantity;
+        })
+        // Sum
+        .reduce((a, b) => a + b);
+    },
+  },
 };
