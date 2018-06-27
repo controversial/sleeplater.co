a8920f9d285128996377a9e911a36b2be866f973
Luke Taylor <luke@deentaylor.com>
2018-06-26 23:58:09 -0400

    refactor some stuff with a helpful productsInCart computed prop
    productsInCart aggregates stuff in an easily digestible format

--- src/components/cart/script.js
+++ src/components/cart/script.js
@@ -7,14 +7,22 @@ export default {
         .map(item => item.quantity)
         .reduce((a, b) => a + b, 0);
     },
-    subtotal() {
+    productsInCart() {
       return this.$store.state.cart
-        // Calculate price for each cart item
         .map(({ id, option, quantity }) => {
           const product = this.$store.state.products.find(p => p.id === id);
-          const optionPrice = product.options[option];
-          return optionPrice * quantity;
-        })
+          return {
+            ...product,
+            option,
+            price: product.options[option],
+            quantity,
+          };
+        });
+    },
+    subtotal() {
+      return this.productsInCart
+        // Calculate price for each cart item
+        .map(({ price, quantity }) => price * quantity)
         // Sum
         .reduce((a, b) => a + b, 0);
     },
