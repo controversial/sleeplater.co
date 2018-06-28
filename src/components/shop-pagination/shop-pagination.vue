e1abe7b95dc631baa1ae7ba91965d40b45f5a824
Luke Taylor <luke@deentaylor.com>
2018-06-28 00:16:19 -0400

    fix errors clicking on middle pagination number
    Closes #10

--- src/components/shop-pagination/shop-pagination.vue
+++ src/components/shop-pagination/shop-pagination.vue
@@ -10,7 +10,6 @@
         right: n >= rightVal, // Right?
         visible: n >= leftVal && n <= rightVal // If it's not either left, center, or right, hide it
       }"
-      v-on:click="change(n)"
     >{{(n + 1).toString().padStart(2, '0')}}</div>
 
     <div class="arrow left" v-bind:class="{ hidden: !canGoLeft }"></div>
