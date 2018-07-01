ffb66718524fbb8b4160e1b767ca0e96a08d656d
Luke Deen Taylor <luke@deentaylor.com>
2018-07-01 15:59:31 -0400

    Finish color-select, with moving checkmark and v-model compatibility

--- src/components/product-options/color-select/color-select.vue
+++ src/components/product-options/color-select/color-select.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="colors">
-    <div class="color" v-for="color in colors" v-bind:style="{ background: color }"></div>
+    <div
+      class="color"
+      v-for="(color, i) in colors" v-bind:key="i"
+      v-bind:style="{ background: color }"
+      v-on:click="$emit('input', color)"
+    ></div>
+
+    <check-icon
+      class="selected-indicator"
+      v-if="colors.indexOf(value) >= 0"
+      v-bind:style="{ left: `${.6 + (1.66 * colors.indexOf(value))}rem` }"
+    ></check-icon>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['colors'],
+  props: ['colors', 'value'], // value is the selected color
 };
 </script>
 
