d96be4e832f1c08a47df521a5fbedf8d6e19af92
Luke Deen Taylor <luke@deentaylor.com>
2018-07-01 15:20:01 -0400

    begin color select component

---
+++ src/components/product-options/color-select/color-select.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="colors">
+    <div class="color" v-for="color in colors" v-bind:style="{ background: color }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['colors'],
+};
+</script>
+
+<style scoped lang="sass" src="./style.sass"></style>
--- src/components/product-options/script.js
+++ src/components/product-options/script.js
@@ -1,3 +1,9 @@
+import Vue from 'vue';
+
+import ColorSelect from './color-select/color-select.vue';
+Vue.component('color-select', ColorSelect);
+
+
 export default {
   props: ['open', 'product'],
 };
