1281c1cd4051f2ec9a9f2539a254f01dc7ce05ee
Luke Deen Taylor <luke@deentaylor.com>
2018-07-01 21:42:30 -0400

    add quantity-select component

---
+++ src/components/product-options/quantity-select/quantity-select.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="quantity-select">
+    <minus-icon v-on:click="$emit('input', Math.max(value - 1, 0))"></minus-icon>
+    <span>{{value}}</span>
+    <plus-icon v-on:click="$emit('input', value + 1)"></plus-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+};
+</script>
+
+<style scoped lang="sass" src="./style.sass"></style>
--- src/components/product-options/script.js
+++ src/components/product-options/script.js
@@ -5,6 +5,8 @@ import ColorSelect from './color-select/color-select.vue';
 Vue.component('color-select', ColorSelect);
 import SizeSelect from './size-select/size-select.vue';
 Vue.component('size-select', SizeSelect);
+import QuantitySelect from './quantity-select/quantity-select.vue';
+Vue.component('quantity-select', QuantitySelect);
 
 
 export default {
@@ -14,15 +16,18 @@ export default {
     productInCart() { return this.$store.state.cart.find(({ id }) => id === this.product.id); },
     colorInCart() { return (this.productInCart || {}).color; },
     sizeInCart() { return (this.productInCart || {}).size; },
+    quantityInCart() { return this.productInCart ? this.productInCart.quantity : 0; },
   },
 
   data: () => ({
     selectedColor: undefined,
     selectedSize: undefined,
+    selectedQuantity: 0,
   }),
 
   beforeMount() {
     this.selectedColor = this.colorInCart;
     this.selectedSize = this.sizeInCart;
+    this.selectedQuantity = this.quantityInCart;
   },
 };
