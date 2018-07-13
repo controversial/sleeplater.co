4d49616804a3055707d2fc307a48021facc278f0
Luke Deen Taylor <luke@deentaylor.com>
2018-07-13 16:08:38 -0400

    Use vue-analytics to record pageview events for each vue-router navigation

--- src/plugins.js
+++ src/plugins.js
@@ -1,6 +1,16 @@
 /* eslint-disable import/first */
 import Vue from 'vue';
 
+
+// VueAnalytics for Google Analytics
+import VueAnalytics from 'vue-analytics';
+Vue.use(VueAnalytics, {
+  id: 'UA-122351714-1',
+  router: require('./router').default,
+});
+
+
+// Tippy for nice tooltips
 import VueTippy from 'vue-tippy';
 Vue.use(VueTippy, {
   // Bound popper.js elements to the viewport instead of the default behavior of the scroll parent
