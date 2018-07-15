4cdce736f8e7f1a49987f91f2e41756af2c90487
Luke Deen Taylor <luke@deentaylor.com>
2018-07-14 21:23:04 -0400

    add imgur module to backend but this is NOT getting done now bc DEADLINES

---
+++ backend/modules/imgur.js
@@ -0,0 +1,5 @@
+const imgur = require('imgur');
+
+// Load environment variables from .env file
+// Should define IMGUR_CLIENT_ID, IMGUR_USERNAME, and IMGUR_PASSWORD for this module
+require('dotenv').config();
