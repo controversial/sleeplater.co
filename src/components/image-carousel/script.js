1ee07b65800d7f320e94446ee7f4f526cd8415ce
Luke Taylor <luke@deentaylor.com>
2018-07-19 11:26:49 -0400

    add left/right/change methods to image-carousel

--- src/components/image-carousel/script.js
+++ src/components/image-carousel/script.js
@@ -1,3 +1,5 @@
+import { clamp } from '../../helpers';
+
 export default {
   props: ['images', 'value'],
 
@@ -7,5 +9,8 @@ export default {
         transform: `translateY(-50%) translate(calc((100% + 7vh) * ${i - this.value}))`,
       };
     },
+    change(i) { this.$emit('input', clamp(i, 0, this.images.length - 1)); },
+    left() { this.change(this.value - 1); },
+    right() { this.change(this.value + 1); },
   },
 };
