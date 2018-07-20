1ac9a3ee36a6c045aa87ebdfd75a1cf3677299f2
Luke Taylor <luke@deentaylor.com>
2018-07-19 22:31:44 -0400

    Add functional pagination underneath image carousel

--- src/components/image-carousel/image-carousel.vue
+++ src/components/image-carousel/image-carousel.vue
@@ -4,6 +4,14 @@
       <img v-for="(url, i) in images" v-bind:style="imageStyle(i)" v-bind:src="url" alt="Product images">
       <div class="image-missing" v-if="!images.length">No image</div>
     </div>
+    <div class="pagination">
+      <div
+        class="dot"
+        v-for="(_, i) in images"
+        v-on:click="change(i)"
+        v-bind:class="{ selected: value === i }"
+      ></div>
+    </div>
   </div>
 </template>
 
