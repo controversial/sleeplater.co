8f70f9a967dc7b6299d1a72fc98367b6f1f6ad7c
Luke Taylor <luke@deentaylor.com>
2018-07-19 23:31:22 -0400

    Use new method of centering product-options
    flexbox!! no longer creates a new stacking context bc no longer has a 
    transform

--- src/components/product-options/product-options.vue
+++ src/components/product-options/product-options.vue
@@ -1,20 +1,22 @@
 <template>
   <transition name="fade">
-    <div class="product-options modal" v-if="$store.state.productsFetched">
-      <div class="left">
-        <image-carousel v-bind:images="product.images[selectedColor] || []" v-model="carouselIndex"></image-carousel>
-      </div>
-      <div class="right">
-        <h1>{{ product.name }}&nbsp;<span class="price">${{ formatPrice(product.price, true) }}</span></h1>
-        <div class="description">{{ product.description }} </div>
-        <color-select v-if="product.options.colors.length" v-bind:colors="product.options.colors" v-model="selectedColor"></color-select>
-        <size-select v-if="product.options.sizes.length" v-bind:sizes="soldOut ? [] : product.options.sizes" v-model="selectedSize"></size-select>
+    <div class="product-options wrapper" v-if="$store.state.productsFetched">
+      <div class="modal">
+        <div class="left">
+          <image-carousel v-bind:images="product.images[selectedColor] || []" v-model="carouselIndex"></image-carousel>
+        </div>
+        <div class="right">
+          <h1>{{ product.name }}&nbsp;<span class="price">${{ formatPrice(product.price, true) }}</span></h1>
+          <div class="description">{{ product.description }} </div>
+          <color-select v-if="product.options.colors.length" v-bind:colors="product.options.colors" v-model="selectedColor"></color-select>
+          <size-select v-if="product.options.sizes.length" v-bind:sizes="soldOut ? [] : product.options.sizes" v-model="selectedSize"></size-select>
 
-        <cart-update-bar
-          v-on:click="addToCart"
-          v-bind:buttonDisabled="buttonDisabled"
-          v-model="selectedQuantity"
-        >{{ buttonMessage }}</cart-update-bar>
+          <cart-update-bar
+            v-on:click="addToCart"
+            v-bind:buttonDisabled="buttonDisabled"
+            v-model="selectedQuantity"
+          >{{ buttonMessage }}</cart-update-bar>
+        </div>
       </div>
     </div>
   </transition>
