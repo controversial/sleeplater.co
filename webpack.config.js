619023f4923038637b41d65b6809a27af240620c
Luke Deen Taylor <luke@deentaylor.com>
2018-08-01 00:34:20 -0400

    add enough babelfication that the site's js will parse in IE11

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!vue2-hammer)/,
       },
       // HTML files
       {
