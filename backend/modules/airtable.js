4d02afa88ac11fd5b269c15df17e5b0d0f74d457
Luke Deen Taylor <luke@deentaylor.com>
2018-08-04 13:40:03 -0400

    retrieve all records from airtable base

--- backend/modules/airtable.js
+++ backend/modules/airtable.js
@@ -6,4 +6,31 @@ const Airtable = require('airtable');
 const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
 module.exports.getProducts = async function getProducts() {
+  // Retrive all the info we'll need from the Airtable API. It's split across 3 tables: Products,
+  // Inventory, and Images.
+
+  const productRecords = [];
+  const inventoryRecords = [];
+  const imageRecords = [];
+
+  await Promise.all([
+    base('Products').select({ view: 'Gallery' })
+      .eachPage((records, fetchNextPage) => {
+        productRecords.push(...records);
+        fetchNextPage();
+      }),
+
+    base('Inventory').select({ view: 'Spreadsheet view' })
+      .eachPage((records, fetchNextPage) => {
+        inventoryRecords.push(...records);
+        fetchNextPage();
+      }),
+
+    base('Images').select({ view: 'Grid view' })
+      .eachPage((records, fetchNextPage) => {
+        imageRecords.push(...records);
+        fetchNextPage();
+      }),
+  ]);
+  return [productRecords, inventoryRecords, imageRecords];
 };
