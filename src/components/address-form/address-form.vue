5b41ca82d2918370b2fcd17bef863475b70e3e58
Luke Deen Taylor <luke@deentaylor.com>
2018-08-05 17:39:44 -0400

    fill up address-form

--- src/components/address-form/address-form.vue
+++ src/components/address-form/address-form.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="form">
+    <div class="row">
+      <text-input title="Full name" v-model="name" :required="true" v-bind="{ showValidation }"></text-input>
+    </div>
+    <div class="row">
+      <text-input title="Street address" v-model="streetAddress" :required="true" v-bind="{ showValidation }"></text-input>
+    </div>
+    <div class="row">
+      <text-input title="City" :required="true" v-model="city" v-bind="{ showValidation }"></text-input>
+      <dropdown title="Country / region" :required="true" v-model="country" v-bind:options="countries" v-bind="{ showValidation }"></dropdown>
+    </div>
+    <div class="row">
+      <text-input title="ZIP/Postal code" :required="true" v-model="zip" v-bind="{ showValidation }"></text-input>
+      <text-input title="State / Territory" :required="true" v-model="state" v-bind="{ showValidation }"></text-input>
+    </div>
+    <div class="cash-region-error" v-if="$store.state.paymentMethod === 'cash' && zip.length >= 5 && !zipValid">
+      Looks like you entered an address for which cash payments aren’t available. Cash payments are
+      negotiated in person and orders are hand-delivered to accomadate local customers with no other
+      way to pay. This service is only available in the regions covered by the United States ZIP
+      codes { cashZipCodes.slice(0, -1).join(', ') } and { cashZipCodes.slice(-1)[0] }.
+    </div>
   </div>
 </template>
 
--- src/components/address-form/script.js
+++ src/components/address-form/script.js
@@ -1,2 +1,32 @@
+const countries = require('country-list')().getNames();
+
+
 export default {
+  props: ['show-validation'],
+
+  data: () => ({
+    countries,
+    cashZipCodes: [12561, 12525, 12528, 12486, 12472, 12548, 12515],
+    name: '',
+    streetAddress: '',
+    city: '',
+    country: '',
+    zip: '',
+    state: '',
+  }),
+
+  computed: {
+    address() {
+      const { name, line1, line2, city, country, zip, state } = this; // eslint-disable-line object-curly-newline, max-len
+      return [
+        name,
+        line1,
+        line2,
+        `${city}, ${state} ${zip}`,
+        country,
+      ];
+    },
+
+    zipValid() { return this.cashZipCodes.includes(parseInt(this.zip, 10)); },
+  },
 };
